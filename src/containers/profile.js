7d57618f9ce5427fd3b3087760893e08bc0fdb47
Bruce Conner <bruce.conner.ii@gmail.com>
2018-06-29 22:32:04 -0700

    Pulling Presentational Logic out of Container
    Pulling presentational logic out of profile container and into its
    component.

--- src/containers/profile.js
+++ src/containers/profile.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
+import Profile from "../components/profile.js"
 
-export default class Profile extends Component {
+export default class ProfileContainer extends Component {
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
 		return (
-			<div className="site-body-wrapper">
-				<div className="profile">
-					<div className="profile__image-wrapper">
-						<img className="profile__image" src="" alt=""/>
-					</div>
-					<div className="profile__information">
-						<p className="profile__username"></p>
-
-						<p className="profile__label">First Name:</p>
-						<p className="profile__first-name"></p>
-
-						<p className="profile__label">Favorite Character:</p>
-						<p className="profile__favorite-character"></p>
-
-						<p className="profile__label"></p>
-						<p className="profile__user-description"></p>
-					</div>
-				</div>
-			</div>
+			<div></div>
 		);
 	}
+}
+
+function mapStateToProps(state) {
+	return {
+		
+	};
 }
